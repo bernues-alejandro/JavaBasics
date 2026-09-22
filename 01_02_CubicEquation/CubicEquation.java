82317118786e63835c0c17a5eac3b632fa83cc83
Alejandro <bernuesalejandro@gmail.com>
2026-09-22 10:53:46 +0200

    exercici 02 encara no acabat

---
+++ 01_02_CubicEquation/CubicEquation.java
@@ -0,0 +1,17 @@
+public class CubicEquation {
+
+	public static void main(String[] args) {
+		int x = 3;
+		int f = x^3 + 2*x^2 + 4*x + 5;
+		System.out.println("f(" + x + ") = " + f);	
+
+        x = 5;
+		f = x^3 + 2*x^2 + 4*x + 5;;
+		System.out.println("f(" + x + ") = " + f);	
+		
+		x = 10;
+		f = ;
+		System.out.println("f(" + x + ") = " + f);	
+
+	}
+}
