868bf99327786cee78bc83c7be37c91935015427
Alejandro <bernuesalejandro@gmail.com>
2026-09-25 14:25:01 +0200

    exercici completat

---
+++ exercicisClasse/exercici1.java
@@ -0,0 +1,13 @@
+public class exercici1 {
+	public static void main(String[] args) {
+		double variable = 9.0;
+		
+		double resultat = Math.pow(variable, 10.0);
+		double resultat2 = Math.sqrt(variable);
+		
+		System.out.println("Resultat 1 = " + resultat);
+		System.out.println("Resultat 2 = " + resultat2);
+		
+		double numRandom = Math.random();
+	}
+}
